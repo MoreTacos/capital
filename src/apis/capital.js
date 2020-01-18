5348cf73b0ea6e397c5e3c137581d746bc8c53d3
Davide Radaelli <daviderad@outlook.com>
2020-01-17 20:43:22 -0500

    added comments

--- src/apis/capital.js
+++ src/apis/capital.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 const capital = axios.create({
   baseURL: 'https://restcountries.eu/rest/v2/all'
 });
-
+// TEST
 export default capital;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './componants/App';
-
 import axios from 'axios';
 const val = axios.create({
   baseURL: 'https://restcountries.eu/rest/v2/all'
 });
+
 ReactDOM.render(<App />, document.querySelector('#root'));
