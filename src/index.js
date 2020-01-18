39f201763147f4bbc280287f2768b81ccad37779
Davide Radaelli <daviderad@outlook.com>
2020-01-18 18:25:13 -0500

    fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './componants/App';
-import axios from 'axios';
 import './App.scss';
-const val = axios.create({
-  baseURL: 'https://restcountries.eu/rest/v2/all'
-});
-
 ReactDOM.render(<App />, document.querySelector('#root'));
